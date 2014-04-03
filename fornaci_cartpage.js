820a716f94414ca467e14f7216139bee9429d99b
susanna pepi <susannapepi@gmail.com>
2014-04-03 12:54:42 +0200

    Delete fornaci_cartpage.js

--- fornaci_cartpage.js
+++
@@ -1,8 +0,0 @@
-function onAddProductSuccess1MS(id , childProductId, quantity,product){
-console.log(id);
-console.log(childProductId);
-console.log(quantity);
-console.log(product);
-alert('Il prodotto è stato aggiunto al carrello. Verrai reindirizzato al checkout in pochi secondi.');
-window.location="/carrello.html";
-};
