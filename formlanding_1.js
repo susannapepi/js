e7a91530bd2d58a254c5a37de797bd28fe582049
susanna pepi <susannapepi@gmail.com>
2014-04-18 16:55:02 +0200

    Create formlanding_1.js

---
+++ formlanding_1.js
@@ -0,0 +1,4 @@
+        jQuery(function(){
+           jsonForm = {"fields":[{"tipo":"Text","options":{"label":"Nome","required":1}},{"tipo":"Text","options":{"label":"Email","required":1}},{"tipo":"Text","options":{"label":"Oggetto","required":0}},{"tipo":"TextArea","options":{"label":"Contenuto","required":1}}]};
+           printContactForm('contact_content_1001', jsonForm);
+        });
