9c109024ed9499e6bcddad00a658237ee7ade6f2
susanna pepi <susannapepi@gmail.com>
2014-04-22 14:21:11 +0200

    Update buttonlanding.js

--- buttonlanding.js
+++ buttonlanding.js
@@ -2,6 +2,4 @@ $(document).ready(function(){
   
   //cambio il testo del pulsante invia
   
-$('.page-type-6 button#invia').html(function() {
-    return $(this).text().replace('Invia', 'Richiedi Informazioni');
-});
+document.getElementsByIdName("invia")[0].innerHTML = "Richiedi Informazioni";
