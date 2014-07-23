82480872e6e80efbec38c4634b6d1c1df506810e
susanna pepi <susannapepi@gmail.com>
2014-07-23 10:28:01 +0200

    Update buttonstext.js

--- buttonstext.js
+++ buttonstext.js
@@ -4,6 +4,6 @@ $(document).ready(function(){
 //cambio il testo dell'aggiungi al carrello
   
   $(".product-add-to-cart a span span").text("Prenotazione");
-  $(".bottone_checkout  span span").text("Prenota");
+  $(".bottone_checkout span span, .bottone_ordina span span").text("Prenota");
 
 });
