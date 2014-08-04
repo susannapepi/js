ed9b53d5b45f91523a2bcfd7d192c49fbe2fb7ac
susanna pepi <susannapepi@gmail.com>
2014-08-04 11:55:53 +0200

    Update filter.js

--- filter.js
+++ filter.js
@@ -6,13 +6,13 @@
                         tagPrefix: 'C-',        // prefisso usato nei tag per individuare i valori di questo filtro
                         label: 'Categoria',     
                         code: 'cat',            // codice del filtro, viene messo come "name" nella select del filtro e nel div contenitore del filtro, con questa convenzione: "filter-CODICE". Può essere utile per interventi puntuali su singoli filtri
-                        noSelectedText:'Tutte'  // testo da mostrare di default nella tendina (se omesso viene messo a vuoto)
+                        noSelectedText:'tutte'  // testo da mostrare di default nella tendina (se omesso viene messo a vuoto)
                     },
                     {
                         tagPrefix: 'M-',
-                        label: 'Marchio',
-                        code: 'marchio',
-                        noSelectedText:'Tutti'
+                        label: 'Marca',
+                        code: 'marca',
+                        noSelectedText:'tutte'
                     }
                 ]
             }
@@ -21,7 +21,7 @@
                 ProductFilter.generate();
 
                 //qui è possibile inserire traduzioni diverse per i test del filtro
-                translations["label.ecommerce.product-filter.fulltext_search"]="Nome prodotto";
-                translations["button.ecommerce.product-filter.filter"]="Filtra";
+                translations["label.ecommerce.product-filter.fulltext_search"]="Inserisci il nome del prodotto che stai cercando";
+                translations["button.ecommerce.product-filter.filter"]="Cerca";
                 translations["label.ecommerce.product-filter.no_results"]="Nessun prodotto corrispondente ai criteri di ricerca";
             }
