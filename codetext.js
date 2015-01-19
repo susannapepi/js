d4bf7481e254f255796d06ec6ce96c2f3c87be20
susanna pepi <susannapepi@gmail.com>
2015-01-19 23:26:58 +0100

    Update codetext.js

--- codetext.js
+++ codetext.js
@@ -2,4 +2,4 @@
   
   $(".product_code").html($(".product_code").html().replace("Codice:",""))
 
-  $(".product-item a.more").text("scopri il programma");
+  $("a.more").text("scopri il programma");
