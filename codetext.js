4779dcb7db19afb33e5df18dc020c98fafed23f7
susanna pepi <susannapepi@gmail.com>
2015-01-19 14:29:00 +0100

    Update codetext.js

--- codetext.js
+++ codetext.js
@@ -1,7 +1,4 @@
-//lo eseguo dopo che ho caricato il sito
-$(document).ready(function(){
-
-//cambio il testo dell'aggiungi al carrello
+//rimuovo la scritta Codice:
   
   $(".product_code").html($(".product_code").html().replace("Codice:",""))
 
