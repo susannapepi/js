abe2a916468af4053895e98440431557ce6353f4
susanna pepi <susannapepi@gmail.com>
2015-01-21 00:20:19 +0100

    Update codetext.js

--- codetext.js
+++ codetext.js
@@ -1,8 +1,7 @@
 //modifiche testi:
   
 $("a.more").text("scopri il programma");
-$(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
-$(".product_code").html($(".product_code").html().replace("Codice:",""));
-$(function() {
-
-});
+$(".bd-last-minute .product_price").html(
+	$(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
+$(".product_code").html(
+	$(".product_code").html().replace("Codice:",""))
