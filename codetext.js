939465ef7b1b5b404365035b097aebf863f17622
susanna pepi <susannapepi@gmail.com>
2015-01-20 23:52:01 +0100

    Update codetext.js

--- codetext.js
+++ codetext.js
@@ -1,5 +1,6 @@
 //rimuovo la scritta Codice:
 $("a.more").text("scopri il programma");
+$(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
 
   $(function() {
   $(".product_code").html($(".product_code").html().replace("Codice:",""));
@@ -11,6 +12,6 @@ $("a.more").text("scopri il programma");
   
 //modifico la scritta prezzo:
 
-  $(function() {
+
   $(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
-});
+
