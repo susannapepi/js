728157d7a55b9dabe47ab868bc8ef0bc2eb8881b
susanna pepi <susannapepi@gmail.com>
2015-01-21 00:30:12 +0100

    Update mosogliotext.js

--- mosogliotext.js
+++ mosogliotext.js
@@ -1,3 +1,3 @@
 //modifiche testi
-
 $(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
+$("a.more").text("scopri il programma");
