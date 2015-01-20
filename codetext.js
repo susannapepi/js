a70e0c0a1a540dd7f955c71c07460195d7727b06
susanna pepi <susannapepi@gmail.com>
2015-01-21 00:03:13 +0100

    Update codetext.js

--- codetext.js
+++ codetext.js
@@ -1,8 +1,5 @@
 //modifiche testi:
-$(document).ready(function(){
-  
-$(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
-$("a.more").text("scopri il programma");
-$(".product_code").html($(".product_code").html().replace("Codice:",""));
 
-});
+$("a.more").text("scopri il programma");  
+$(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
+$(".product_code").html($(".product_code").html().replace("Codice:",""));
