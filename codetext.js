286928cdf4dd21b338301e09804833a24c878f13
susanna pepi <susannapepi@gmail.com>
2015-01-21 00:31:28 +0100

    Update codetext.js

--- codetext.js
+++ codetext.js
@@ -1,3 +1,5 @@
 //rimuovo il testo codice:
   
+$(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
 $(".product_code").html($(".product_code").html().replace("Codice:",""));
+$("a.more").text("scopri il programma");
