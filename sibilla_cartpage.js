c873059ba8a777bde7d04dd4895bd28cf09be7d1
susanna pepi <susannapepi@gmail.com>
2015-01-21 15:11:45 +0100

    Update sibilla_cartpage.js

--- sibilla_cartpage.js
+++ sibilla_cartpage.js
@@ -1,9 +1,14 @@
 function onAddProductSuccess1MS(id , childProductId, quantity,product){
-if (siteLanguage.code == 'it'){
-  alert("Il prodotto è stato aggiunto al carrello. Verrai reindirizzato al checkout in pochi secondi.") ;
-}else if(siteLanguage.code == 'en'){
-   alert("The product has been added to cart. You will be redirected to the checkout in a few seconds.")
-}
 
-window.location="/carrello.html";
+var destUrl = null;
+var msg = null;
+if (siteLanguage.code == 'it'){
+  destUrl = "/carrello.html";
+  msg = "Il prodotto è stato aggiunto al carrello. Verrai reindirizzato al checkout in pochi secondi.";
+}else if(siteLanguage.code == 'en'){
+   destUrl = "/cart.html";
+  msg = "The product has been added to cart. You will be redirected to the checkout in a few seconds.";
+}
+  alert(msg);
+  window.location=Oms.Util.getLanguageBaseUrl(destUrl);
 };
