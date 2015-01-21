659a63c04f73a307e98e985d502d54e6e666c352
susanna pepi <susannapepi@gmail.com>
2015-01-21 09:58:51 +0100

    Update codetext.js

--- codetext.js
+++ codetext.js
@@ -1,6 +1,11 @@
-//rimuovo il testo codice:
+//lo eseguo dopo che ho caricato il sito
+$(document).ready(function(){
   
-$(".product_code").html($(".product_code").html().replace("Codice:",""));
+//cambio il testo dell'aggiungi al carrello
 
+$(".product_code").html($(".product_code").html().replace("Codice:",""));
 $(".bd-last-minute .product_price").html($(".bd-last-minute .product_price").html().replace("Prezzo:","A partire da:"));
 $("a.more").text("scopri il programma");
+
+  
+});
